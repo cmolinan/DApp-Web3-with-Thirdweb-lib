8599b3bb067ba87688a50c4016b9d950d9665fce
Carlos Molina <cmolinan@gmail.com>
2024-06-28 15:11:36 -0500

    Added more CSS Styling to Login Modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,41 +1,51 @@
 "use client"
-import { useState } from 'react';
 
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button, Input } from 'antd';
 
 import './LoginModal.css';
 
-const LoginModal = ({ onLogin }) => {
+const LoginModal = ({ onLogin, isAuth }) => {
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
+  const [loading1, setLoading1] = useState(false);
 
   const handleClickToLogin = () => {
     // Validar que se ingresen el usuario y la contraseña
     if (user.trim() === '' || password.trim() === '') {
       return;
     }
-    onLogin(user, password);
+    isAuth ? setLoading1(false):setLoading1(true)
+    onLogin(user, password);    
   };
 
   return (
     <div className="login-modal">
       <div className="login-form">
-        <h2>Iniciar Sesión</h2>
-        <label htmlFor="user">Usuario:</label>
+        <h2>INICIAR SESION</h2>
+        <label htmlFor="user">Usuario o e-mail:</label>        
         <input
-          type="text"
+          placeholder="Ingresa e-mail o usuario"
+          type='text'
+          allowClear
           id="user"
           value={user}
-          onChange={(e) => setUser(e.target.value)}
+          size='small'
+          onChange={(e) => setUser(e.target.value)}          
         />
+        <br />
         <label htmlFor="password">Contraseña:</label>
         <input
+          placeholder="Ingresa password"
           type="password"
           id="password"
+          allowClear
           value={password}
+          size='small'
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button onClick={handleClickToLogin}>Ingresar</button>
+        <Button size='large' onClick={handleClickToLogin} style={{fontSize: '18px', fontWeight:'bold'}} >INGRESAR</Button>
       </div>
     </div>
   );
